d510b23c2cd94bb1150330a57b8b1d685be915ae
Nhat <minhnhatngo700@gmail.com>
2023-11-04 22:04:15 +0700

    Thêm category

--- DOAN2/script.js
+++ DOAN2/script.js
@@ -34,7 +34,7 @@ function next(){
 }
 function prev(){
     slides[index].classList.remove('active');
-    index = (index - 1) % slides.length;
+    index = (index - 1 + slides.length) % slides.length;
      slides[index].classList.add('active');
     // if(index<=0){
     //     slides[index].classList.add('active');
